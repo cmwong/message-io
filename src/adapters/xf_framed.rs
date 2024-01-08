29726968790176a85ecd9b78894f401c422ae429
cmwong <cheemon_wong@yahoo.com>
2024-01-08 15:56:05 +0800

    xf_framed

--- src/adapters/xf_framed.rs
+++ src/adapters/xf_framed.rs
@@ -6,8 +6,8 @@ use crate::network::adapter::{
     ListeningInfo, PendingStatus,
 };
 use crate::network::{RemoteAddr, Readiness, TransportConnect, TransportListen};
-use crate::util::encoding::{self, Decoder, MAX_ENCODED_SIZE};
-
+// use crate::util::encoding::{self, Decoder};
+pub const MAX_ENCODED_SIZE: usize = 4;
 use mio::net::{TcpListener, TcpStream};
 use mio::event::{Source};
 
@@ -123,7 +123,7 @@ impl Remote for RemoteResource {
                 }
                 Err(err) => {
                     log::error!("TCP receive error: {}", err);
-                    break ReadStatus::Disconnected // should not happen
+                    break ReadStatus::Disconnected; // should not happen
                 }
             }
         }
@@ -131,7 +131,7 @@ impl Remote for RemoteResource {
 
     fn send(&self, data: &[u8]) -> SendStatus {
         let mut buf = [0; MAX_ENCODED_SIZE]; // used to avoid a heap allocation
-        let encoded_size = encoding::encode_size(data, &mut buf);
+        let encoded_size = encode_size(data, &mut buf);
 
         let mut total_bytes_sent = 0;
         let total_bytes = encoded_size.len() + data.len();
@@ -146,13 +146,13 @@ impl Remote for RemoteResource {
                 Ok(bytes_sent) => {
                     total_bytes_sent += bytes_sent;
                     if total_bytes_sent == total_bytes {
-                        break SendStatus::Sent
+                        break SendStatus::Sent;
                     }
                 }
                 Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => continue,
                 Err(err) => {
                     log::error!("TCP receive error: {}", err);
-                    break SendStatus::ResourceNotFound // should not happen
+                    break SendStatus::ResourceNotFound; // should not happen
                 }
             }
         }
@@ -223,9 +223,114 @@ impl Local for LocalResource {
     }
 }
 
-
-fn encode_size<'a>(message: &[u8], buf: &'a mut [u8; 4]) -> &'a [u8] {
+fn encode_size<'a>(message: &[u8], buf: &'a mut [u8; MAX_ENCODED_SIZE]) -> &'a [u8] {
     let size = message.len() as u32 + 4;
-    let variant_size = size.encode_fixed(buf);
-    &buf[..4]
+    let _ = size.encode_fixed(buf);
+    &buf[..MAX_ENCODED_SIZE]
+}
+fn decode_size(data: &[u8]) -> usize {
+    u32::decode_fixed(data) as usize
+}
+
+struct Decoder {
+    stored: Vec<u8>,
+}
+
+impl Default for Decoder {
+    /// Creates a new decoder.
+    /// It will only reserve memory in cases where decoding needs to keep data among messages.
+    fn default() -> Decoder {
+        Decoder { stored: Vec::new() }
+    }
+}
+
+impl Decoder {
+    fn try_decode(&mut self, data: &[u8], mut decoded_callback: impl FnMut(&[u8])) {
+        let mut next_data = data;
+        loop {
+            // if let Some((expected_size, used_bytes)) = decode_size(next_data) {
+            //     let remaining = &next_data[used_bytes..];
+            //     if remaining.len() >= expected_size {
+            //         let (decoded, not_decoded) = remaining.split_at(expected_size);
+            //         decoded_callback(decoded);
+            //         if !not_decoded.is_empty() {
+            //             next_data = not_decoded;
+            //             continue;
+            //         } else {
+            //             break;
+            //         }
+            //     }
+            // }
+            let expected_size = decode_size(next_data);
+            let remaining = &next_data[..];
+            if remaining.len() >= expected_size {
+                let (decoded, not_decoded) = remaining.split_at(expected_size);
+                decoded_callback(decoded);
+                if !not_decoded.is_empty() {
+                    next_data = not_decoded;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            self.stored.extend_from_slice(next_data);
+        }
+    }
+
+    fn store_and_decoded_data<'a>(&mut self, data: &'a [u8]) -> Option<(&[u8], &'a [u8])> {
+        // Process frame header
+        let ((expected_size, used_bytes), data) = match decode_size(&self.stored) {
+            Some(size_info) => (size_info, data),
+            None => {
+                // we append at most the potential data needed to decode the size
+                let max_remaining = (MAX_ENCODED_SIZE - self.stored.len()).min(data.len());
+                self.stored.extend_from_slice(&data[..max_remaining]);
+
+                if let Some(x) = decode_size(&self.stored) {
+                    // Now we know the size
+                    (x, &data[max_remaining..])
+                } else {
+                    // We still don't know the size (data was too small)
+                    return None;
+                }
+            }
+        };
+
+        // At this point we know at least the expected size of the frame.
+        let remaining = expected_size - (self.stored.len() - used_bytes);
+        if data.len() < remaining {
+            // We need more data to decoder
+            self.stored.extend_from_slice(data);
+            None
+        } else {
+            // We can complete a message here
+            let (to_store, remaining) = data.split_at(remaining);
+            self.stored.extend_from_slice(to_store);
+            Some((&self.stored[used_bytes..], remaining))
+        }
+    }
+
+    /// Tries to decode data without reserve any memory, direcly from `data`.
+    /// `decoded_callback` will be called for each decoded message.
+    /// If `data` is not enough to decoding a message, the data will be stored
+    /// until more data is decoded (more successives calls to this function).
+    pub fn decode(&mut self, data: &[u8], mut decoded_callback: impl FnMut(&[u8])) {
+        if self.stored.is_empty() {
+            self.try_decode(data, decoded_callback);
+        } else {
+            //There was already data in the Decoder
+            if let Some((decoded_data, remaining)) = self.store_and_decoded_data(data) {
+                decoded_callback(decoded_data);
+                self.stored.clear();
+                self.try_decode(remaining, decoded_callback);
+            }
+        }
+    }
+
+    /// Returns the bytes len stored in this decoder.
+    /// It can include both, the padding bytes and the data message bytes.
+    /// After decoding a message, its bytes are removed from the decoder.
+    pub fn stored_size(&self) -> usize {
+        self.stored.len()
+    }
 }
